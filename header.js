fb140248278dc2bccbcf44b44a77f7e5ee9746e2
alvingogo <accctw376@gmail.com>
2026-04-01 17:25:33 +0800

    [2026] remove comment

--- header.js
+++ header.js
@@ -1,12 +1,10 @@
 (function() {
-    // 1. 定義需要載入的 JS (順序很重要)
     const scripts = [
         "https://code.jquery.com/jquery-3.3.1.slim.min.js",
         "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js",
         "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
     ];
 
-    // 2. 定義你的 HTML 內容 (把原本的 var a 搬過來)
     var navbarHTML = `
         <header>
             <section id='main'>
@@ -61,7 +59,6 @@
             </div>
         </header>`;
 
-    // 3. 遞迴載入腳本的函數
     function loadScripts(index) {
         if (index < scripts.length) {
             let script = document.createElement('script');
@@ -70,24 +67,18 @@
             script.onload = () => loadScripts(index + 1);
             document.head.appendChild(script);
         } else {
-            // 4. JS 載入完畢後，初始化 Navbar
             initNavbar();
         }
     }
 
     function initNavbar() {
-        // 使用 document.write 或是插入特定 div
-        // 如果你的 HTML 裡有 <div id="header"></div> 就用 innerHTML
-        // 如果沒有，維持 document.write (但建議改用插入的方式)
         const headerDiv = document.getElementById('header');
         if (headerDiv) {
             headerDiv.innerHTML = navbarHTML;
         } else {
-            // 如果沒找到容器，只好暴力插入到 body 開頭
             document.body.insertAdjacentHTML('afterbegin', navbarHTML);
         }
 
-        // 5. 重要：手動啟動 Bootstrap 選單功能
         $(document).ready(function() {
             $('.dropdown-toggle').dropdown();
             $('.navbar-toggler').on('click', function() {
@@ -96,7 +87,6 @@
         });
     }
 
-    // 啟動執行
     if (document.readyState === 'loading') {
         window.addEventListener('DOMContentLoaded', () => loadScripts(0));
     } else {
